dc856842b0ccdec2d44c97725125f238f6b89ff7
Justin Ladwig <justin.ladwig.2003@gmail.com>
2020-12-02 20:22:57 +0100

    Styling Code

--- software/Co2.ino
+++ software/Co2.ino
@@ -31,8 +31,8 @@
 #define BRIGHTNESS 255
 
 //Definieren der Farben
-#define COLOR_GREEN 65536/3
-#define COLOR_YELLOW 65536/6
+#define COLOR_GREEN 65536 / 3
+#define COLOR_YELLOW 65536 / 6
 #define COLOR_RED 0
 #define COLOR_BLUE 65536 * 2 / 3
 
@@ -41,14 +41,30 @@
 #define CO2_WARNING 1000
 #define CO2_BAD 2000
 
+#define CO2_DEFAULT 400
+
 #define CO2_ACC_ALERT 1800
 #define CO2_VIS_ALERT 1500
+
+#define CO2_LOOP_DELAY 1000
+
+#define CO2_DIFFERENCE 500
+#define CO2_MEASURE_AMOUNT 5
+#define CO2_MEASURE_DELAY 1000
+
+#define BEEP_DURATION 1000
+#define BEEP_AMOUNT 2
+#define BLINK_DURATION 1000
+#define BLINK_AMOUNT 5
 
 //Deklariern des Led-Objekts
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
 
 //Deklariern des CO2-Sensor-Objekts
 Adafruit_CCS811 ccs;
+
+//Deklarieren des letzten CO2 Werts
+int lastECO2 = CO2_DEFAULT;
 
 /*Notizen
 
@@ -58,120 +74,119 @@ Adafruit_CCS811 ccs;
 
 */
 
-void setup() {
+void setup()
+{
   WiFi.mode(WIFI_OFF);
   Serial.begin(115200);
 
   pinMode(PIEZO_PIN, OUTPUT);
   pinMode(SENSOR_WAKE_PIN, OUTPUT);
 
+
   //Leds starten
   strip.begin();
   strip.setBrightness(BRIGHTNESS);
   strip.show();
 
-
   digitalWrite(SENSOR_WAKE_PIN, LOW);
   //Sensor starten
-  if (!ccs.begin()) {
+  if (!ccs.begin())
+  {
     Serial.println("Failed to start sensor! Please check your wiring.");
     while (1);
   }
 }
 
-void loop() {
+void loop()
+{
+  int eCO2 = checkMeasurementECO2();
+  Serial.print("Measurement: ");
+  Serial.println(eCO2);
+  uint32_t color = getColor(eCO2);
+  strip.fill(color, 0, LED_COUNT);
+  strip.show();
 
-  if (ccs.available()) {
-    if (!ccs.readData()) {
-      int eCO2raw = ccs.geteCO2();
-      int eCO2 = constrain(eCO2raw, CO2_GOOD, CO2_BAD);
-      Serial.print(eCO2raw);
-      Serial.println(" ppm");
-      strip.fill(getColor(eCO2), 0, LED_COUNT);
+  if (eCO2 >= CO2_ACC_ALERT)
+  {
+    for (int i = 0; i < BEEP_AMOUNT; i++)
+    {
+      digitalWrite(PIEZO_PIN, HIGH);
+      delay(BEEP_DURATION);
+      digitalWrite(PIEZO_PIN, LOW);
+      delay(BEEP_DURATION);
+    }
+  }
+
+  if (eCO2 >= CO2_VIS_ALERT)
+  {
+    for (int i = 0; i < BLINK_AMOUNT; i++)
+    {
+      strip.clear();
       strip.show();
+      delay(BLINK_DURATION);
+      strip.fill(color, 0, LED_COUNT);
+      strip.show();
+      delay(BLINK_DURATION);
+    }
+  } else
+  {
+    delay(CO2_LOOP_DELAY);
+  }
+  
 
-      if (eCO2 >= CO2_ACC_ALERT) {
-        delay(50);
-        alert();
-      } else {
-        delay(50);
-      }
-    } else {
-      //Fehler
+}
+
+int checkMeasurementECO2()
+{
+  int eCO2;
+  for (int i = 0; i < CO2_MEASURE_AMOUNT; i++)
+  {
+    eCO2 = measureECO2();
+    if (abs(eCO2 - lastECO2) <= CO2_DIFFERENCE && eCO2 != 0 )
+    {
+      break;
+    }
+    delay(CO2_MEASURE_DELAY);
+  }
+
+  lastECO2 = eCO2;
+
+  if (eCO2 == 0)
+  {
+    lastECO2 = CO2_DEFAULT;
+    return 0;
+  }
+  return constrain(eCO2, CO2_GOOD, CO2_BAD);
+}
+
+int measureECO2()
+{
+  if (ccs.available())
+  {
+    if (!ccs.readData())
+    {
+      Serial.println(ccs.geteCO2());
+      return ccs.geteCO2();
+    }
+    else
+    {
       Serial.println("ERROR!");
     }
   }
-
-}
-
-void alert() {
-  while (1) {
-    if (ccs.available()) {
-      if (!ccs.readData()) {
-        int eCO2raw = ccs.geteCO2();
-      int eCO2 = constrain(eCO2raw, CO2_GOOD, CO2_BAD);
-        Serial.print(eCO2raw);
-        Serial.println(" ppm");
-        if (eCO2 < CO2_ACC_ALERT) {
-          break;
-        }
-        uint32_t color = getColor(eCO2);
-
-        strip.fill(getColor(eCO2), 0, LED_COUNT);
-        strip.show();
-        digitalWrite(PIEZO_PIN, HIGH);
-
-        delay(500);
-
-        strip.clear();
-        strip.show();
-
-        delay(500);
-
-        digitalWrite(PIEZO_PIN, LOW);
-        strip.fill(getColor(eCO2), 0, LED_COUNT);
-        strip.show();
-
-        delay(500);
-
-        strip.clear();
-        strip.show();
-
-        delay(500);
-
-        digitalWrite(PIEZO_PIN, HIGH);
-        strip.fill(getColor(eCO2), 0, LED_COUNT);
-        strip.show();
-
-        delay(500);
-
-        strip.clear();
-        strip.show();
-
-        delay(500);
-
-        digitalWrite(PIEZO_PIN, LOW);
-        for (int i = 0; i < 1; i++) {
-          strip.fill(getColor(eCO2), 0, LED_COUNT);
-          strip.show();
-          delay(500);
-          strip.clear();
-          strip.show();
-          delay(500);
-        }
-
-      } else {
-        //Fehler
-      }
-    }
+  else
+  {
+    return 0;
   }
 }
 
-
-uint32_t getColor(int eCO2) {
-  if (eCO2 >= CO2_GOOD && eCO2 <= CO2_WARNING) {
+uint32_t getColor(int eCO2)
+{
+  if (eCO2 >= CO2_GOOD && eCO2 <= CO2_WARNING)
+  {
     return strip.ColorHSV(map(eCO2, CO2_GOOD, CO2_WARNING, COLOR_GREEN, COLOR_YELLOW));
-  } else if (eCO2 >= CO2_WARNING && eCO2 <= CO2_BAD) {
+  }
+  else if (eCO2 >= CO2_WARNING && eCO2 <= CO2_BAD)
+  {
     return strip.ColorHSV(map(eCO2, CO2_WARNING, CO2_BAD, COLOR_YELLOW, COLOR_RED));
   }
   return COLOR_BLUE;
